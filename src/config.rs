c1a43e6f6f955c1fc45ac3ddb707219ed281ddb9
haozixu <haozixu2001@qq.com>
2022-01-25 08:39:09 +0800

    generate image output

--- src/config.rs
+++ src/config.rs
@@ -6,3 +6,6 @@ extern crate nalgebra as na;
 pub type Fp = f64;
 
 pub type Vector3f = Vector3<Fp>;
+
+pub const IMG_WIDTH: u32 = 300;
+pub const IMG_HEIGHT: u32 = 200;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 mod config;
 mod material;
 
+extern crate image;
+extern crate itertools;
+
+use config::{IMG_WIDTH, IMG_HEIGHT};
+use image::{ImageBuffer, Rgb, RgbImage};
+use rayon::prelude::*;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut output_path = "output.jpg";
+
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 {
+        output_path = &args[1];
+    }
+
+    let mut buf: RgbImage = ImageBuffer::new(IMG_WIDTH, IMG_HEIGHT);
+    let points: Vec<_> = itertools::iproduct!(0..IMG_WIDTH, 0..IMG_HEIGHT).collect();
+    let results: Vec<_> = points.par_iter().map(|&(x, y)| {
+        ((x, y), [x as u8, y as u8, (x + y) as u8])
+    }).collect();
+
+    for ((x, y), pixel) in results {
+        buf.put_pixel(x, y, Rgb(pixel));
+    }
+    buf.save(output_path).unwrap()
 }
